821043f8ad2903c1e259e90d1c72c0808f4a70df
langlang <2289816251@qq.com>
2023-06-27 01:12:53 +0800

    初始化服务器

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
-// 引入express
+// 引入 Express
 const express = require('express')
+
+// 引入 Cors
+const cors = require('cors')
 
 // 创建app
 const app = express()
+
+// 配置跨域
+app.use(cors)
 
 // 监听窗口
 app.listen(8080,()=>{
