1d3b2467edb618ec5b59800edb7a5e77b9818cb4
langlang <2289816251@qq.com>
2023-07-06 18:54:38 +0800

    初始化项目

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ const app = express()
 // 配置跨域
 app.use(cors)
 
+// 引入路由
+const index = require('./router/index')
+
+// 配置路由
+app.use('/index',index)
+
 // 监听窗口
 app.listen(8080,()=>{
     console.log('服务器启动中 地址：http://127.0.0.1:8000')
